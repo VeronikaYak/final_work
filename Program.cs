692510a15ffc9994b89af4ed55458fffd3dfe49f
Veronika Yakimuk <v.yakimuk@tut.by>
2024-04-01 16:18:40 +0300

    доработан файл Program.cs в части вывода данных

--- Program.cs
+++ Program.cs
@@ -29,3 +29,19 @@ string PrintArray(string[] workArray)
     stringArray += "]";
     return stringArray;
 }
+
+string PrintArray(string[] workArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (i == workArray.Length - 1)
+        {
+            stringArray += $"\"{workArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{workArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
